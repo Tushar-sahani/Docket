b82a5f48ec4aeec96b605213294582d5c07eb9d0
Tushar Sahani <104075614+Tushar-sahani@users.noreply.github.com>
2024-04-29 19:45:28 +0530

    todo app

---
+++ src/components/CourseGoalList.tsx
@@ -0,0 +1,39 @@
+import CourseGoal from './CourseGoal.tsx';
+import { type CourseGoal as CGoal } from '../App.tsx';
+import InfoBox from './InfoBox.tsx';
+import { ReactNode } from 'react';
+
+type CourseGoalListProps = {
+  goals: CGoal[];
+  onDeleteGoal: (id: number) => void;
+};
+
+export default function CourseGoalList({
+  goals,
+  onDeleteGoal,
+}: CourseGoalListProps) {
+
+  if(goals.length===0){
+    return <InfoBox mode='hint'>You have no goals yet, Start adding some!</InfoBox>
+  }
+  
+  let warning:ReactNode;
+
+  if(goals.length === 4){
+    warning = <InfoBox mode='warning'>Your'r collecting a lot of goals. Don't put too much on your plate!</InfoBox>
+  }
+  return (
+    <>
+    {warning}
+    <ul>
+      {goals.map((goal) => (
+        <li key={goal.id}>
+          <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
+            <p>{goal.description}</p>
+          </CourseGoal>
+        </li>
+      ))}
+    </ul>
+    </>
+  );
+}
